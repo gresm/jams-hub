e50a99cc97c9788bbf05aeaf1ab40f12499e610e
gresm <78505251+gresm@users.noreply.github.com>
2022-06-25 12:06:08 +0200

    Add engine tools, credits to gresm.

---
+++ engine/window.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from typing import Callable
+from warnings import warn
+
+import pygame as pg
+
+
+pg.init()
+
+
+class GameState:
+    def __init__(self, fps: int):
+        self.max_fps = fps
+        self.clock = pg.time.Clock()
+        self.running = True
+        self._frame: Callable[[pg.Surface, float], ...] | None = None
+
+    def stop(self):
+        self.running = False
+
+    def frame(self, func: Callable[[pg.Surface, float], ...]):
+        self._frame = func
+        return func
+
+    def run(self):
+        while self.running:
+            if self._frame is not None:
+                if max_fps != -1:
+                    ms = self.clock.tick(max_fps)
+                else:
+                    ms = self.clock.tick()
+                self._frame(screen, ms / self.max_fps)  # probably you are missing "window" and "delta_time" arguments
+            else:
+                warn("Running without specified frame executor")
+                break
+
+
+size = (800, 800)
+max_fps = 60
+screen = pg.display.set_mode(size)
+game = GameState(max_fps)
+
+__all__ = [
+    "size",
+    "max_fps",
+    "screen",
+    "game"
+]
