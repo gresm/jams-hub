8338f216dd6c30f20648bf010cb2f14abf9a1718
gresm <78505251+gresm@users.noreply.github.com>
2022-06-26 17:05:29 +0200

    Use frame counters.

--- game_jams/jam_1/scenes.py
+++ game_jams/jam_1/scenes.py
@@ -9,15 +9,11 @@ import pygame as pg
 
 
 class MainMenu(BaseScene):
-    frame_counter: int
-    seconds_counter: int
     color_iter: Callable[[], tuple[int, int, int]]
 
     def init(self):
         pg.display.set_caption("Top and Bottom - Main Menu")
         self.color_iter = color_permutations.color_iter()
-        self.frame_counter = 0
-        self.seconds_counter = 0
 
     def update(self):
         for event in self.get_events():
@@ -26,12 +22,9 @@ class MainMenu(BaseScene):
                     self.manager.spawn_scene(GameScene)
                 elif event.key == pg.K_ESCAPE:
                     self.manager.game.stop()
-        self.frame_counter += 1
-        if self.frame_counter == 60:
-            self.frame_counter = 0
-            self.seconds_counter += 1
-            if self.seconds_counter % 5 == 0:
-                self.color_iter = color_permutations.color_iter()
+
+        if self.frame_counter.seconds % 5 == 0 and self.frame_counter.new_second:
+            self.color_iter = color_permutations.color_iter()
 
     def draw(self, surface: pg.Surface):
         surface.fill(self.color_iter())
@@ -40,16 +33,22 @@ class MainMenu(BaseScene):
         surface.blit(assets.font_text.render("Press Escape to quit", True, (255, 255, 255)), (10, 80))
 
 
+class LevelSelectionScene(BaseScene):
+    def init(self):
+        pg.display.set_caption("Top and Bottom - Level Selection")
+
+    def update(self):
+        for event in self.get_events():
+            if event.type == pg.KEYDOWN:
+                pass
+
+
 class GameScene(BaseScene):
-    frame_counter: int
-    seconds_counter: int
     upper_scene: BaseScene | None
     background_color_iter: Callable[[], tuple[int, int, int]]
 
     def init(self):
         pg.display.set_caption("Top and Bottom - Game")
-        self.frame_counter = 0
-        self.seconds_counter = 0
         self.upper_scene = None
         self.background_color_iter = color_permutations.color_iter()
 
@@ -62,12 +61,8 @@ class GameScene(BaseScene):
                     else:
                         self.manager.game.stop()
 
-        self.frame_counter += 1
-        if self.frame_counter == 60:
-            self.frame_counter = 0
-            self.seconds_counter += 1
-            if self.seconds_counter % 5 == 0:
-                self.background_color_iter = color_permutations.color_iter()
+        if self.frame_counter.seconds % 5 == 0 and self.frame_counter.new_second:
+            self.background_color_iter = color_permutations.color_iter()
 
     def draw(self, surface: pg.Surface):
         surface.fill(self.background_color_iter())
