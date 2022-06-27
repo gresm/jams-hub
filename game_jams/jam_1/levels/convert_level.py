2d71298dc3889f8abe1d18441886793ebdf388f1
gresm <78505251+gresm@users.noreply.github.com>
2022-06-27 09:57:47 +0200

    Collision masks.

--- game_jams/jam_1/levels/convert_level.py
+++ game_jams/jam_1/levels/convert_level.py
@@ -1,8 +1,20 @@
 """
 Convert RawLevel to pymunk space for physics simulation.
 """
-from .raw_level import RawLevel
+from raw_level import RawLevel
+from enum import Enum
 import pymunk
+
+
+class CollisionMasks(Enum):
+    players = 1
+    walls = 4
+    goals = 8
+    movable = 16
+    level_boundary = 32
+    movable_go_through_boundary = 64
+    player_collide_with = 125  # walls | movable | goals | level_boundary | movable_go_through_boundary | players
+    movable_collide_with = 53  # walls | movable | players | level_boundary
 
 
 class PymunkLevel:
